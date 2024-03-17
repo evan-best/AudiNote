e43b8f0ccf1cf7fd89a62b3c6ccb59269b1a7c51
evan-best <117332327+evan-best@users.noreply.github.com>
2024-03-17 15:16:24 -0230

    Initial Commit

---
+++ AudiNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudiNote
+//
+//  Created by Evan Best on 2024-03-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
